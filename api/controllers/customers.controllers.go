f4852f0b786b6bb51ea1fa28d4c9693776784eb7
stergiosbamp <bampakis.stergios@gmail.com>
2023-04-11 18:02:48 +0300

    Customer endpoints use DAO pattern

--- api/controllers/customers.controllers.go
+++ api/controllers/customers.controllers.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/stergiosbamp/go-api/database"
+	"github.com/stergiosbamp/go-api/dao"
+	"github.com/stergiosbamp/go-api/models"
 )
 
-var db, _ = database.GetDB()
+var customerDAO = dao.NewCustomerDAO()
 
-type Customer struct {
+type CustomerRequest struct {
 	ID                 uint  	 `json:"id" binding:"numeric"`
 	Active             *bool   	 `json:"active" binding:"required,boolean"`
 	Name               string 	 `json:"name" binding:"required,max=100"`
@@ -30,23 +31,22 @@ type URI struct {
 	ID  uint `uri:"id" binding:"required"`
 }
 
-type PatchActiveBody struct {
+type CustomerPatchRequest struct {
 	Active             *bool   	 `json:"active" binding:"required,boolean"`
 }
 
 func GetCustomer(ctx *gin.Context) {
 	var uri URI
-	var customer Customer
 
 	if err := ctx.ShouldBindUri(&uri); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	
 	id := uri.ID
+	customer, err := customerDAO.GetById(id)
 	
-	res := db.Where("id = ?", id).First(&customer)
-	
-	if res.Error != nil {
+	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Customer with ID %v not found.", id)})
 		return
 	}
@@ -55,12 +55,10 @@ func GetCustomer(ctx *gin.Context) {
 }
 
 func GetCustomers(ctx *gin.Context) {
-	var customers []Customer
+	customers, err := customerDAO.GetList()
 	
-	res := db.Find(&customers)
-	
-	if res.Error != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", res.Error)})
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", err.Error())})
 		return
 	}
 
@@ -68,103 +66,158 @@ func GetCustomers(ctx *gin.Context) {
 }
 
 func CreateCustomer(ctx *gin.Context) {
-	var customer Customer
+	var customerReq CustomerRequest
 
-	if err := ctx.ShouldBindJSON(&customer); err != nil {
+	if err := ctx.ShouldBindJSON(&customerReq); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	// Populate a Model from request data
+	customer := &models.Customer{
+		Active: *customerReq.Active,
+		Name: customerReq.Name,
+		ShortName: customerReq.ShortName,
+		VatNumber: customerReq.VatNumber,
+		VatApply: *customerReq.VatApply,
+		RegistrationNumber: customerReq.RegistrationNumber,
+		DunsNumber: customerReq.DunsNumber,
+		TaxExempt: *customerReq.TaxExempt,
+		Language: customerReq.Language,
+		Email: customerReq.Email,
+		Phone: customerReq.Phone,
+		Fax: customerReq.Fax,
+	}
 	
-	res := db.Create(&customer)
-	
-	if res.Error != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", res.Error)})
+	customerCreated, err := customerDAO.Create(customer)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", err.Error())})
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, customer)
+	ctx.JSON(http.StatusCreated, customerCreated)
 }
 
-func UpdateOrCreateCustomer(ctx *gin.Context) {
+func UpdateCustomer(ctx *gin.Context) {
 	var uri URI
-	var updatedCustomer Customer
 
 	if err := ctx.ShouldBindUri(&uri); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	
-	if err := ctx.ShouldBindJSON(&updatedCustomer); err != nil {
+	var customerReq CustomerRequest
+
+	if err := ctx.ShouldBindJSON(&customerReq); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	// Assign the ID for Update or Create
-	updatedCustomer.ID = uri.ID
-
-	res := db.Save(&updatedCustomer)
 	
-	if res.Error != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", res.Error)})
+	// Check if customer to update exists
+	_, err := customerDAO.GetById(uri.ID)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Customer with id: %v doesn't exist", uri.ID)})
+		return		
+	}
+
+	// Populate a Model with the id path parameter and the updated request data.
+	// If in the future, PUT can work without requiring all fields, then use the "oldCustomer" data from the above operation of DAO 
+	// in conjunction with the "customerReq" data to fill up the model for the missing fields.
+	customer := &models.Customer{
+		ID: uri.ID, // inject ID for update
+		Active: *customerReq.Active,
+		Name: customerReq.Name,
+		ShortName: customerReq.ShortName,
+		VatNumber: customerReq.VatNumber,
+		VatApply: *customerReq.VatApply,
+		RegistrationNumber: customerReq.RegistrationNumber,
+		DunsNumber: customerReq.DunsNumber,
+		TaxExempt: *customerReq.TaxExempt,
+		Language: customerReq.Language,
+		Email: customerReq.Email,
+		Phone: customerReq.Phone,
+		Fax: customerReq.Fax,
+	}
+
+	customerUpdated, err := customerDAO.Update(customer)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", err.Error())})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, updatedCustomer)
+	ctx.JSON(http.StatusCreated, customerUpdated)
 }
 
 func PatchCustomer(ctx *gin.Context) {
 	var uri URI
-	var customer Customer
-	var patchBody PatchActiveBody
 
 	if err := ctx.ShouldBindUri(&uri); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	id := uri.ID
-	res := db.Where("id = ?", id).First(&customer)
-	
-	if res.Error != nil{
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Couldn't find customer with ID %v", id)})
-		return
-	}
-	
-	// ADD BUSINESS LOGIC THAT CANNOT ACTIVATE A CUSTOMER WITHOUT HAVING IN DB A LEGAL ADDRESS.
-	
-	if err := ctx.BindJSON(&patchBody); err != nil {
+	var patchBody CustomerPatchRequest
+
+	if err := ctx.ShouldBindJSON(&patchBody); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Update his status
-	customer.Active = patchBody.Active
+	// Check if customer to update exists
+	oldCustomer, err := customerDAO.GetById(uri.ID)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Customer with id: %v doesn't exist", uri.ID)})
+		return		
+	}
+
+	// ADD BUSINESS LOGIC THAT CANNOT ACTIVATE A CUSTOMER WITHOUT HAVING IN DB A LEGAL ADDRESS.
 	
-	res = db.Save(&customer)
-	if res.Error != nil{
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Couldn't find customer with ID %v", id)})
+	customerPatched := models.Customer{
+		ID: uri.ID, // inject ID
+		Active: *patchBody.Active, // update status
+		Name: oldCustomer.Name,
+		ShortName: oldCustomer.ShortName,
+		VatNumber: oldCustomer.VatNumber,
+		VatApply: oldCustomer.VatApply,
+		RegistrationNumber: oldCustomer.RegistrationNumber,
+		DunsNumber: oldCustomer.DunsNumber,
+		TaxExempt: oldCustomer.TaxExempt,
+		Language: oldCustomer.Language,
+		Email: oldCustomer.Email,
+		Phone: oldCustomer.Phone,
+		Fax: oldCustomer.Fax,
+	}
+	
+	newCustomer, err := customerDAO.Update(&customerPatched)
+	
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", err.Error())})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, customer)
+	ctx.JSON(http.StatusOK, newCustomer)
 }
 
 func DeleteCustomer(ctx *gin.Context) {
 	var uri URI
-	var customer Customer
 
 	if err := ctx.ShouldBindUri(&uri); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	id := uri.ID
-
-	res := db.Where("id = ?", id).Unscoped().Delete(&customer)
-	if res.Error != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", res.Error)})
-		return
+	_, err := customerDAO.GetById(uri.ID)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Customer with id: %v doesn't exist", uri.ID)})
+		return		
 	}
 
-	ctx.JSON(http.StatusNoContent, customer)
+	errDeleted := customerDAO.Delete(uri.ID)
+	if errDeleted != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Error DB. Full error %v", err.Error())})
+		return
+	}
+	ctx.JSON(http.StatusNoContent, gin.H{"":""})
 }
