4b03b995b34d5e1fcb17bfab419f148983d2812b
stergiosbamp <bampakis.stergios@gmail.com>
2023-04-12 17:05:51 +0300

    Customer model also has json tags for response with omitempty

--- api/models/customer.models.go
+++ api/models/customer.models.go
@@ -1,24 +1,24 @@
 package models
 
 type Customer struct {
-	ID                 uint  
-	Active             bool   	 `gorm:"NOT NULL"`
-	Name               string 	 `gorm:"NOT NULL"`
-	ShortName          string	 `gorm:"NOT NULL"`
-	VatNumber          string	 `gorm:"NOT NULL"`
-	VatApply           bool	 	 `gorm:"NOT NULL"`
-	RegistrationNumber string	 `gorm:"NOT NULL"`
-	DunsNumber         string	 `gorm:"NOT NULL"`
-	TaxExempt          bool   	 `gorm:"NOT NULL"`
-	Language           string	 `gorm:"size:2;NOT NULL"`
-	Email              string	 `gorm:"NOT NULL"`
-	Phone              string	 `gorm:"NOT NULL"`
-	Fax                string	 `gorm:"NOT NULL"`
-	
+	ID                 uint   `json:"id"`
+	Active             bool   `gorm:"NOT NULL" json:"active"`
+	Name               string `gorm:"NOT NULL" json:"name"`
+	ShortName          string `gorm:"NOT NULL" json:"shortName"`
+	VatNumber          string `gorm:"NOT NULL" json:"vatNumber"`
+	VatApply           bool   `gorm:"NOT NULL" json:"vatApply"`
+	RegistrationNumber string `gorm:"NOT NULL" json:"registrationNumber"`
+	DunsNumber         string `gorm:"NOT NULL" json:"dunsNumber"`
+	TaxExempt          bool   `gorm:"NOT NULL" json:"taxExempt"`
+	Language           string `gorm:"size:2;NOT NULL" json:"language"`
+	Email              string `gorm:"NOT NULL" json:"email"`
+	Phone              string `gorm:"NOT NULL" json:"phone"`
+	Fax                string `gorm:"NOT NULL" json:"fax"`
+
 	/*
-	The following slices are not needed to model the the DB as it is originally.
+		The following slices are not needed to model the the DB as it is originally.
 	*/
-	
+
 	// Addresses 		   []Address `gorm:"NOT NULL"`
 	// Contacts           []Contact `gorm:"NOT NULL"`
 }
